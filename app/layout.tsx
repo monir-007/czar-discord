4378272dcdbc300a2df35edc36132279fa231a28
Monir <monirwahid40@gmail.com>
2023-09-05 00:23:36 +0600

    feat(mode): dark default mode

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,8 @@ import type {Metadata} from 'next'
 import {Open_Sans} from 'next/font/google'
 import React from "react";
 import {ClerkProvider} from '@clerk/nextjs';
+import {ThemeProvider} from "@/components/providers/theme-provider";
+import {cn} from "@/lib/utils";
 
 const font = Open_Sans({subsets: ['latin']})
 
@@ -18,8 +20,19 @@ export default function RootLayout({
 }) {
     return (
         <ClerkProvider>
-            <html lang="en">
-            <body className={font.className}>{children}</body>
+            <html lang="en" suppressHydrationWarning>
+            <body className={cn(
+                font.className,
+                "bg-white dark:bg-[#313338]"
+                )}>
+            <ThemeProvider
+                attribute="class"
+                defaultTheme="dark"
+                enableSystem={false}
+                storageKey="czar-theme">
+                {children}
+            </ThemeProvider>
+            </body>
             </html>
         </ClerkProvider>
 
